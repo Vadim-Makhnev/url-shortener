764c0a1b115335929731c97def1a4aa1234b975a
Vadim <vadik.mahnev@yandex.ru>
2025-11-11 22:25:43 +0300

    feat:add methods for repository.go

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -3,11 +3,17 @@ package repository
 import (
 	"context"
 	"database/sql"
+	"errors"
 	"fmt"
 	"os"
 	"time"
 
+	_ "github.com/lib/pq"
 	"github.com/redis/go-redis/v9"
+)
+
+var (
+	ErrNotFound = errors.New("url not found")
 )
 
 type URL struct {
@@ -56,4 +62,79 @@ func NewRepository() (*Repository, error) {
 		db:    db,
 		redis: redisClient,
 	}, nil
+}
+
+func (r *Repository) CreateURL(url *URL) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	query := `INSERT INTO urls (short_code, original_url) VALUES ($1, $2)
+			RETURNING id, created_at`
+
+	err := r.db.QueryRowContext(ctx, query, url.ShortCode, url.OriginalURL).Scan(&url.ID, &url.CreatedAt)
+	if err != nil {
+		return fmt.Errorf("repository: CreateURL: %w", err)
+	}
+	return nil
+}
+
+func (r *Repository) GetURLByShortCode(shortCode string) (*URL, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	cachedURL, err := r.redis.Get(ctx, shortCode).Result()
+	if err == nil {
+		return &URL{
+			ShortCode:   shortCode,
+			OriginalURL: cachedURL,
+		}, nil
+	}
+
+	var url URL
+	query := `SELECT id, short_code, original_url, created_at, access_count
+			FROM urls WHERE short_code = $1`
+
+	err = r.db.QueryRowContext(ctx, query, shortCode).Scan(&url.ID, &url.ShortCode, &url.OriginalURL, &url.CreatedAt, &url.AccessCount)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, ErrNotFound
+		}
+		return nil, fmt.Errorf("repository: GetURLByShortCode: %w", err)
+	}
+
+	r.redis.Set(ctx, shortCode, url.OriginalURL, 24*time.Hour)
+
+	return &url, nil
+}
+
+func (r *Repository) GetAllURLS() ([]URL, error) {
+	query := `SELECT id, short_code, original_url, created_at, access_count FROM urls ORDER BY
+	created_at DESC`
+
+	rows, err := r.db.Query(query)
+	if err != nil {
+		return nil, fmt.Errorf("repository: GetAllURLS: %w", err)
+	}
+	defer rows.Close()
+
+	var urls []URL
+	for rows.Next() {
+		var url URL
+		if err := rows.Scan(&url.ID, &url.ShortCode, &url.OriginalURL, &url.CreatedAt, &url.AccessCount); err != nil {
+			return nil, err
+		}
+		urls = append(urls, url)
+	}
+
+	return urls, nil
+}
+
+func (r *Repository) IncrementAccessCount(shortCode string) error {
+	query := `UPDATE urls SET access_count = access_count + 1 WHERE short_code = $1`
+	_, err := r.db.Exec(query, shortCode)
+	if err != nil {
+		return fmt.Errorf("repository: IncrementAccessCount: %w", err)
+	}
+
+	return nil
 }
