ae983422b0cbab3c532112258404e0550e87e65b
Vadim <vadik.mahnev@yandex.ru>
2025-11-27 20:26:51 +0300

    refactor(redis):change Get method

--- internal/repository/redis.go
+++ internal/repository/redis.go
@@ -29,10 +29,13 @@ func (r *RedisRepository) Get(shortCode string) (string, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	cmd := r.redis.Get(ctx, shortCode)
-	if cmd.Err() != nil {
-		return "", fmt.Errorf("redis: can't get value by key: %s", shortCode)
+	val, err := r.redis.Get(ctx, shortCode).Result()
+	if err != nil {
+		if err == redis.Nil {
+			return "", fmt.Errorf("redis: key not found: %s", shortCode)
+		}
+		return "", fmt.Errorf("redis: can't get value by key %s: %w", shortCode, err)
 	}
 
-	return cmd.Val(), nil
+	return val, nil
 }
